7ca3a87efbfd70f547563d193bcde03a2244aafd
Cutwell <zacharysmith2000@googlemail.com>
2017-06-28 21:38:56 +0100

    altered upload system

--- source/flag_bearer.py
+++ source/flag_bearer.py
@@ -2,33 +2,20 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib
 import tkinter as tk
 import shutil
-import pyre
 import random
 import hashlib
 
-class server_gui(object):
+class server_setup(object):
+    def __init__(self, file, password=None):
+        self.password = password
+        self.name = file
+        self.file = open(file, "rb").read()
 
+class webpage_setup(object):
     def __init__(self):
-        pyre.Pyre()
-        pyre.print("Standard setup:")
-        self.username = pyre.input("Enter Flag name")
-        self.password = pyre.input("Enter Passcode (optional)")
-        pyre.pyre_root.withdraw()
-
-        # create the upload var, so we can access it from any instance of the HTTPServer_RequestHandler class
-        self.upload = ""
-
-    def main(self):
-        self.root = tk.Tk()
-
-
-    def gen_secure_key(self):
-        string = (self.username+str(random.random())).encode('UTF-8')
-        hash_object = hashlib.sha1(string)
-        hash_dig = list(hash_object.hexdigest())
-        self.secure_key = "".join(hash_dig[:6])
-        print("New Secure Key (use to access the /upload webpage): "+self.secure_key)
-
+        self.logo = open("logo.png", "rb").read()
+        self.password = open("pages/password.html").read()
+        self.standard = open("pages/standard.html").read()
 
 class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
@@ -38,34 +25,25 @@ class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
         if self.path == "/logo.png" or self.path == "/favicon.ico":
             self.send_header('Content-type', 'image/png')
             self.end_headers()
-            self.wfile.write(open("logo.png", "rb").read())
+            self.wfile.write(html.logo)
             return
 
-        elif self.path == "/download":
-            with open("/upload/flag.txt") as fh:
-                self.send_header('Content-type', 'application/zip')
-                self.end_headers()
-                self.wfile.write(fh.read().encode())
+        elif self.path == user.name:
+            self.send_header('Content-type', 'image/png')
+            self.end_headers()
+            self.wfile.write(user.file)
 
         else:
             self.send_header('Content-type', 'text/html')
             if self.path == "/standard":
-                if gui.password:
-                    webpage = open("pages/password.html").read()
+                if user.password:
+                    webpage = html.password
                     webpage = webpage.replace("%identifier%", "password")
                     webpage = webpage.replace("%require_password_info%", "This Flag is password protected!")
                 else:
-                    webpage = open("pages/standard.html", "r").read()
-                    webpage = webpage.replace("%user_name%", gui.username)
-                    webpage = webpage.replace("%path_to_file%", gui.upload)
-                    webpage = webpage.replace("%size_of_file%", str(len(gui.upload)/1000000))
-                    webpage = webpage.replace("%name_of_file%", gui.upload)
-
-            elif self.path == "/upload":
-                webpage = open("pages/password.html").read()
-                webpage = webpage.replace("%identifier%", "secure_key")
-                webpage = webpage.replace("%require_password_info%", "This area is restricted!")
-
+                    webpage = html.standard
+                    webpage = webpage.replace("%size_of_file%", str(round(len(user.file)/10000, 2)))
+                    webpage = webpage.replace("%name_of_file%", user.name)
             else:
                 webpage = "<div style='color:#c9d0e0; font-family:courier new; text-align:center;'><a href='/standard' title='return to /standard page'><img src='logo.png' style='height: 250px; width: 250px;'></a></img><p style='font-size:50px;'>404 Error!</p><br><p style='font-size:25px;'>Page not found...</p></div>"
 
@@ -77,39 +55,14 @@ class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
         post_length = int(self.headers["Content-Length"])
         post_data = urllib.parse.parse_qs(self.rfile.read(post_length).decode("utf-8"))
 
-        # security feature
-        if "secure_key" in post_data and post_data["secure_key"][0] != str(gui.secure_key):
-            print("Bad Login: User attempted to access /upload page with bad secure_key")
-            gui.gen_secure_key()
-            webpage = "<div style='color:#c9d0e0; font-family:courier new; text-align:center;'><img src='logo.png' style='height: 250px; width: 250px;'></img><p style='font-size:50px;'>500 Error!</p><br><p style='font-size:25px;'>Access denied!</p></div>"
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(bytes(webpage, "utf8"))
-            return
-
-        if "password" in post_data and post_data["password"][0] == str(gui.password):
-            webpage = open("pages/standard.html", "r").read()
-            webpage = webpage.replace("%user_name%", gui.username)
-            webpage = webpage.replace("%path_to_file%", gui.upload)
-            webpage = webpage.replace("%size_of_file%", str(len(gui.upload)/1000000))
-            webpage = webpage.replace("%name_of_file%", gui.upload)
-
-        elif "secure_key" in post_data and post_data["secure_key"][0] == str(gui.secure_key):
-            webpage = open("pages/upload.html").read()
-            gui.gen_secure_key()
-
-        elif "file" in post_data:
-            length = int(self.headers['content-length'])
-            data = self.rfile.read(length)
-
-            with open("/upload/flag.txt", 'w') as file:
-                file.write(data.decode())
-
-            webpage = "<div style='color:#c9d0e0; font-family:courier new; text-align:center;'><img src='logo.png' style='height: 250px; width: 250px;'></img><p style='font-size:50px;'>Upload Succesful!</p><br><p style='font-size:25px;'>Visit /standard to access the file for download</p></div>"
-
+        if "password" in post_data and post_data["password"][0] == str(user.password):
+            webpage = html.standard
+            webpage = webpage.replace("%size_of_file%", str(round(len(user.file)/10000, 2)))
+            webpage = webpage.replace("%name_of_file%", user.name)
+            print("Succesful connection")
         else:
             webpage = "<div style='color:#c9d0e0; font-family:courier new; text-align:center;'><img src='logo.png' style='height: 250px; width: 250px;'></img><p style='font-size:50px;'>500 Error!</p><br><p style='font-size:25px;'>Access denied!</p></div>"
+            print("Unsuccessful connection: Invalid password")
 
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -119,9 +72,12 @@ class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
         return
 
 if __name__ == "__main__":
-    gui = server_gui()
-    gui.gen_secure_key()
+    if sys.argv[1]:
+        user = server_setup(sys.argv[0], sys.argv[1])
+    else:
+        user = server_setup(sys.argv[0])
+    html = webpage_setup()
     server_address = ('127.0.0.1', 8081)
     httpd = HTTPServer(server_address, HTTPServer_RequestHandler)
-    print("server running on localhost:8081/")
+    print("server running on localhost:8081/standard")
     httpd.serve_forever()
